5860f48be61fbb630984e384d6536d2571a3241a
gadiribrahimov <iqq87@yahoo.com>
2022-12-09 22:48:37 -0800

    Create Role and users in Keycloak and Connect to Keycloak and secure roles

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -20,33 +21,38 @@ public class ProjectController {
     }
 
     @GetMapping
-    public ResponseEntity<ResponseWrapper> getProjects() {
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> getProjects(){
         List<ProjectDTO> projectDTOList = projectService.listAllProjects();
-        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projectDTOList, HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projectDTOList, HttpStatus.OK));
     }
 
     @GetMapping("/{code}")
-    public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code) {
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code){
         ProjectDTO projectDTO = projectService.getByProjectCode(code);
-        return ResponseEntity.ok(new ResponseWrapper("Project is successfully retrieved", projectDTO, HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("Project is successfully retrieved",projectDTO, HttpStatus.OK));
 
     }
 
     @PostMapping
-    public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project) {
+    @RolesAllowed({"Admin","Manager"})
+    public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project){
         projectService.save(project);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is successfully created", HttpStatus.CREATED));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Project is successfully created",HttpStatus.CREATED));
     }
 
     @PutMapping
-    public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project) {
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO project){
         projectService.update(project);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully updated", HttpStatus.OK));
 
     }
 
     @DeleteMapping("/{projectcode}")
-    public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("projectcode") String projectcode) {
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> deleteProject(@PathVariable("projectcode") String projectcode){
         projectService.delete(projectcode);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully deleted", HttpStatus.OK));
 
@@ -54,13 +60,15 @@ public class ProjectController {
     }
 
     @GetMapping("/manager/project-status")
-    public ResponseEntity<ResponseWrapper> getProjectByManager() {
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> getProjectByManager(){
         List<ProjectDTO> projectDTOList = projectService.listAllProjectDetails();
-        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projectDTOList, HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved",projectDTOList, HttpStatus.OK));
     }
 
     @PutMapping("/manager/complete/{projectCode}")
-    public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode) {
+    @RolesAllowed("Manager")
+    public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode){
         projectService.complete(projectCode);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully completed", HttpStatus.OK));
 
