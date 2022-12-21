e4f761e07ed6534531b9d42b892bde744c36294c
gadiribrahimov <iqq87@yahoo.com>
2022-12-20 21:14:16 -0800

    Integration testing - ProjectControllerTest, getProject, createProject, updateProject, deleteProject getToken method and TestResponseDTO

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -2,17 +2,25 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.TestResponseDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.enums.Status;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 
@@ -34,8 +42,9 @@ class ProjectControllerTest {
 
     @BeforeAll
     static void setUp() {
-        token = "Bearer " + "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ4QllubzhPUEt5ckwwX2toRHpoZ21zTU1CdkVmZVR3NEVQd1VpTnJKNzY4In0.eyJleHAiOjE2NzE1MzgyMDYsImlhdCI6MTY3MTUyMDIwNiwianRpIjoiNDI2NGEzNDItNTBiMy00NDM1LThiZTMtMjA3YTIzZTc5YWFmIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI4MGUyOTdkYS03ZDhkLTQxM2MtYjFhYS0wYTI2NzAzNDhhYTQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6ImJmYWE2ZGQzLTViMzgtNGFlOC04NjEyLTg4MmI0ZDFjNDMxNSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtY3lkZW8tZGV2IiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiJiZmFhNmRkMy01YjM4LTRhZTgtODYxMi04ODJiNGQxYzQzMTUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoib3p6eSJ9.bQ27I2hDFdszsdrXG0aWBNgddKjFgoBtSMBY3MQRkkreBACxLjoA5Idz4MJveC0rhHVnRln63AiExevxdMVtF-7ScXAXMp1BIK9Iu7vxySoK6sb8iWZpE1FhaC3X1i8wTbe4zFT0_Aj9ayeCC-mVsntjWRbtd64ipLHWbwC7Fj0EeuXj3dQ9_BP_7LUu0JTKlTYWFmbgQ2TIZkhcfPgZ0M6JzsZ5jh8vAJ8ZROJuqZlTyGHZ2bb0UXjUOqOftL5fOYakGXFCtFPP24eF3KA7cNHITGVrwS8vxUspfKwRIiN1WcIZFmc1D-Zy9CGjL5YYmr7HWKjsL4Y8ipIyBPkjkg";
-                manager = new UserDTO(2L,
+        token = "Bearer " + getToken();
+
+        manager = new UserDTO(2L,
                 "",
                 "",
                 "ozzy",
@@ -57,6 +66,7 @@ class ProjectControllerTest {
         );
 
     }
+
     @Test
     void givenNoToken_getProjects() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/api/v1/project"))
@@ -75,6 +85,84 @@ class ProjectControllerTest {
                 .andExpect(jsonPath("$.data[0].assignedManager.userName").isNotEmpty())
                 .andExpect(jsonPath("$.data[0].assignedManager.userName").isString())
                 .andExpect(jsonPath("$.data[0].assignedManager.userName").value("ozzy"));
+    }
+
+    @Test
+    void givenToken_createProject() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/v1/project")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(toJsonString(project)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Project is successfully created"));
+
+    }
+
+    @Test
+    void givenToken_updateProject() throws Exception {
+
+        project.setProjectName("API Project-2");
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/v1/project")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(toJsonString(project)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Project is successfully updated"));
+
+    }
+
+    @Test
+    void givenToken_deleteProject() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.delete("/api/v1/project/" + project.getProjectCode())
+                        .header("Authorization", token)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Project is successfully deleted"));
+
+    }
+
+    private String toJsonString(final Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    private static String getToken() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
+        map.add("grant_type", "password");
+        map.add("client_id", "ticketing-app");
+        map.add("client_secret", "qXijuSoZB4n60RIqYd0L4S8kHOYxZSv6");
+        map.add("username", "ozzy");
+        map.add("password", "abc1");
+        map.add("scope", "openid");
+
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
+
+        ResponseEntity<TestResponseDTO> response =
+                restTemplate.exchange("http://localhost:8080/auth/realms/cydeo-dev/protocol/openid-connect/token",
+                        HttpMethod.POST,
+                        entity,
+                        TestResponseDTO.class);
+
+        if (response.getBody() != null) {
+            return response.getBody().getAccess_token();
+        }
+
+        return "";
+
     }
 
 
